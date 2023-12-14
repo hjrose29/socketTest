05d48b56885c5bdca272c85eaf522199ef90a84d
Henry <henryjrose2@gmail.com>
2023-12-13 19:24:55 -0500

    Working server-client

--- Server.java
+++ Server.java
@@ -1,6 +1,51 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     private ServerSocket server;
     private DataInputStream in;
+    public static final int PORT = 8080;
+    static final String STOP_STRING = "##";
+
+    public Server(){
+        try{
+            server = new ServerSocket(PORT);
+            iniConnections();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void iniConnections() throws IOException{
+        Socket clientSocket = server.accept();
+        in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+        readMessages();
+        close();
+    }
+
+    private void close() {
+        try{
+            in.close();
+            server.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void readMessages() throws IOException{
+        String line = "";
+        while(!line.equals(STOP_STRING)){
+            line = in.readUTF();
+            System.out.println(line);
+        }  
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
 }  
